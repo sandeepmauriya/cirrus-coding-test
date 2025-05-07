157e5682d26d2287a17163db3bfa6f52cddde4a6
sandeepmauriya <sandeepmauriyakit@gmail.com>
2025-05-07 23:08:03 +0100

    backend code added

--- services/backend/src/app.ts
+++ services/backend/src/app.ts
@@ -1,15 +1,31 @@
-import express, { Request, Response } from 'express';
-import makeData from './db/makeData';
+import { Request, Response } from 'express';
 
-const app = express();
+import jsonServer from 'json-server';
 
-app.get('/', (req: Request, res: Response) => {
-  res.json('Hello World');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.get('/meter/:id', (req : Request, res: Response) => {
+  const { id } = req.params;
+
+  const meters = router.db.getState().meters;
+  
+  const meter = meters.filter((meter: {
+    guid: string;
+}) => meter.guid === id);
+
+  if (!meter) console.error(`⚠️  Could not find meter id: ${id}`);
+
+  res.jsonp(meter);
 });
 
+server.use(router);
 
 const APP_PORT = 3000;
 
-app.listen(APP_PORT, () => {
+const app = server.listen(APP_PORT, () => {
   console.log(`Server started on port ${APP_PORT}`);
 });
---
+++ services/backend/src/types/json-server.d.ts
@@ -0,0 +1,4 @@
+declare module 'json-server' {
+    const jsonServer: any;
+    export default jsonServer;
+  }
